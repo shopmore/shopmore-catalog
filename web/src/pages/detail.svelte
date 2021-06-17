cfe03c439bcfdcf9b222674263543b06c40c9a97
lavboj <yurabelavin@icloud.com>
2021-06-17 15:36:49 +0200

    Iniziata la pagina detail

--- web/src/pages/detail.svelte
+++ web/src/pages/detail.svelte
@@ -1,1 +1,26 @@
-Salve sono un prodotto
+<script>
+    import Menu from "./_components/Menu.svelte";
+    const colors = [
+            'primary',
+            'secondary',
+            'success',
+            'danger',
+            'warning',
+            'info',
+            'light',
+            'dark'
+        ];
+    import { Col, Container, Row } from 'sveltestrap';
+    
+</script>
+
+<section class="section-content padding-y">
+    <Container>
+        <Row>
+            <Col xs="3">
+            <Menu />
+            </Col>
+        </Row>
+    </Container>
+</section>
+
